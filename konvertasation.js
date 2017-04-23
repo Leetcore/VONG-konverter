8e0fb81b4f083c7c00ce0091fef48d4e8de86d1a
Alex K <Leetcore@users.noreply.github.com>
2017-04-23 10:28:23 +0200

    struktur

--- konvertasation.js
+++ konvertasation.js
@@ -1,1 +1,30 @@
-// 
+var VONG = {
+  timer: 500,
+  VONGliste: [
+    {deutsch: "eine", vong: "1me"},
+    {deutsch: "ein", vong: "1"]},
+    {deutsch: "von", vong: "vong"},
+    {deutsch: "bist", vong: "bimst"},
+    {deutsch: "vom", vong: "vong"},
+    {deutsch: "von", vong: "vong"},
+    {deutsch: "bin", vong: "bims"},
+    {deutsch: "bist", vong: "bimst"},
+    {deutsch: "mein", vong: "meim"},
+    {deutsch: "sprache", vong: "sprech"},
+    {deutsch: "habe", vong: "han"},
+    {deutsch: "hab", vong: "han"}, 
+  ],
+  start: function () {
+    // finde inhalt
+    VONG.konversator(element)
+    setTimeout(VONG.start, VONG.timer)
+  },
+  konversator: function (element) {
+    if (typeof(element) != 'undefined') { 
+      for (var x = 0; x < VONGliste.length; x++) {
+        element.innerHTML = element[0].innerHTML.split(' '+ VONGliste[x].deutsch +' ').join(' '+ VONGliste[x].vong +' ')
+        element.classList.add('itsVONG')
+      }
+    }
+  }
+}
