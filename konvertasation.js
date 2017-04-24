a33e1607f296f28abd2df40acb225634bb7ada7a
Alex K <Leetcore@users.noreply.github.com>
2017-04-24 21:42:20 +0200

    testing

--- konvertasation.js
+++ konvertasation.js
@@ -15,8 +15,8 @@ var VONG = {
     {deutsch: "was", vong: "watt"},
     {deutsch: "von", vong: "vong"},
     {deutsch: "bist", vong: "bimst"},
-    {deutsch: "vom", vong: "vong"},
     {deutsch: "von", vong: "vong"},
+    {deutsch: "vom", vong: "vong"},
     {deutsch: "bin", vong: "bims"},
     {deutsch: "bist", vong: "bimst"},
     {deutsch: "mein", vong: "meim"},
@@ -24,6 +24,7 @@ var VONG = {
     {deutsch: "ist", vong: "isd"},
     {deutsch: "sprache", vong: "sprech"},
     {deutsch: "habe", vong: "han"},
+    {deutsch: "haben", vong: "hannen"},
     {deutsch: "hab", vong: "han"},
     {deutsch: "hat", vong: "han"},
     {deutsch: "nicht", vong: "nid"},
@@ -39,32 +40,24 @@ var VONG = {
   ],
   start: function () {
     // finde 1 inhalt wo nicht vong ist
-    var element = document.querySelector(VONG.matchList[VONG.counter] +':not(.'+ VONG.className +')')
-    
-    // finde mehr inhalt
-    if (element == null ) {
-      // wieder vong vorne
-      if (VONG.counter == VONG.matchList.length) {
-        VONG.counter = 0
-      } else {
-        // finde nächsten match
-        VONG.counter++
-      }
-    } else {
-      // do magic
-      VONG.konversator(element)
+    for (var matchIndex = 0; matchIndex < VONG.matchList.length; matchIndex++) {
+      setTimeout(() => {
+        var elements = document.querySelectorAll(VONG.matchList[matchIndex] +':not(.'+ VONG.className +')')
+        for (var index = 0; index < elements.length; index++) {
+          // do magic
+          VONG.konversator(elements[index])
+        }
+      }, 0)
     }
-    
+
     // round round baby round round
     setTimeout(VONG.start, VONG.timer)
   },
   konversator: function (element) {
     if (typeof(element) != 'undefined') { 
       for (var x = 0; x < VONG.VONGliste.length; x++) {
-        element.innerHTML = element.innerHTML.split(VONG.VONGliste[x].deutsch.substring(0,1).toUpperCase() + VONG.VONGliste[x].deutsch.substring(1, VONG.VONGliste[x].deutsch.length - 1) +' ').join(VONG.VONGliste[x].vong +' ')
-        element.innerHTML = element.innerHTML.split(VONG.VONGliste[x].deutsch.substring(0,1).toUpperCase() + VONG.VONGliste[x].deutsch.substring(1, VONG.VONGliste[x].deutsch.length - 1) +'.').join(VONG.VONGliste[x].vong +'.')
-        element.innerHTML = element.innerHTML.split(' '+ VONG.VONGliste[x].deutsch +' ').join(' '+ VONG.VONGliste[x].vong +' ')
-        element.innerHTML = element.innerHTML.split(' '+ VONG.VONGliste[x].deutsch +'.').join(' '+ VONG.VONGliste[x].vong +'.')
+        element.innerHTML = element.innerHTML.split(VONG.VONGliste[x].deutsch.substring(0,1).toUpperCase() + VONG.VONGliste[x].deutsch.substring(1, VONG.VONGliste[x].deutsch.length - 1)).join(VONG.VONGliste[x].vong.substring(0,1).toUpperCase() + VONG.VONGliste[x].vong.substring(1, VONG.VONGliste[x].vong.length - 1))
+        element.innerHTML = element.innerHTML.split(' '+ VONG.VONGliste[x].deutsch).join(' '+ VONG.VONGliste[x].vong)
         element.classList.add(VONG.className)
       }
     }
