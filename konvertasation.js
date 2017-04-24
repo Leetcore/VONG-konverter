69f4f301278e2e325c6ef14450240fa945560715
Alex K <Leetcore@users.noreply.github.com>
2017-04-24 10:09:03 +0200

    performance und matching

--- konvertasation.js
+++ konvertasation.js
@@ -1,5 +1,8 @@
 var VONG = {
+  counter: 0,
   timer: 500,
+  className: "itsVONG",
+  matchList: ['h1', 'h2', 'article p', 'p', 'span'],
   VONGliste: [
     {deutsch: "eine", vong: "1me"},
     {deutsch: "ein", vong: "1"]},
@@ -13,17 +16,34 @@ var VONG = {
     {deutsch: "sprache", vong: "sprech"},
     {deutsch: "habe", vong: "han"},
     {deutsch: "hab", vong: "han"}, 
+    {deutsch: "wir", vong: "sims"},
+    {deutsch: "ihr", vong: "seit"},
+    {deutsch: "sie", vong: "sims"}
   ],
   start: function () {
-    // finde inhalt
-    VONG.konversator(element)
+    // finde 1 inhalt
+    var element = document.querySelector(VONG.matchList[VONG.counter] +':not(.'+ VONG.className +')')
+    
+    // finde mehr inhalt wenn 0 inhalt
+    if (element == null ) {
+      // wieder vong vorne
+      if (VONG.counter == VONG.matchList.length) {
+        VONG.counter = 0
+      } else {
+        VONG.counter++
+      }
+    } else {
+      VONG.konversator(element)
+    }
     setTimeout(VONG.start, VONG.timer)
   },
   konversator: function (element) {
     if (typeof(element) != 'undefined') { 
       for (var x = 0; x < VONGliste.length; x++) {
-        element.innerHTML = element[0].innerHTML.split(' '+ VONGliste[x].deutsch +' ').join(' '+ VONGliste[x].vong +' ')
-        element.classList.add('itsVONG')
+        element.textContent = element[0].textContent.split(' '+ VONGliste[x].deutsch +' ').join(' '+ VONGliste[x].vong +' ')
+        element.textContent = element[0].textContent.split(' '+ VONGliste[x].deutsch +'.').join(' '+ VONGliste[x].vong +'.')
+        // element.textContent = element[0].textContent.split(' '+ VONGliste[x].deutsch +'.').join(' '+ VONGliste[x].vong +'.')
+        element.classList.add(VONG.className)
       }
     }
   }
